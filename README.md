7550799c4d9aed574ce100ebbd23bd9eb3a78fbf
edy <edy.sassolino@gmail.com>
2022-07-29 01:34:39 +0900

    Add decimal mode, error trap. Update readme.

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Snapshot for ERC721 with Alchemy Transfers API
 
-This reposity provide snapshot function for ERC721 with Alchemy Transfer API.
+This repositry provides snapshot function for ERC721 with Alchemy Transfer API.
 
 ## How to use
 
-### Preparation
+### Installation
 
 After clone this repositry and change directory:
 ```
@@ -13,34 +13,42 @@ npm install
 
 You have to sign in Alchemy, make the Apps to connection the block chain which the NFT that you want snapshot was deployed and get the endpoint URL and your API KEY!
 
-### Execute
+### Usage
+
+After install, you can enter the following in terminal:
 
 ```
 node index.js
 ```
 
-### Setting
+## Setting
 
-`.env` must be prepared to specify Alchemy url and API KEY
+### 1. `.env`
+
+**`.env` must be prepared to specify Alchemy url and API KEY.**
+
 You can copy sample from `.env.example`.
 
 - `ETHEREUM_URL` : ethereum URL and API KEY
 - `POLYGON_URL` : polygon URL and API KEY
 
+### 2. `snapshot.config.json`
+This is the setting file.
 
-`snapshot.config.json` is the setting file.
-
-- `chain` : snapshot chain, "ethereum" and "polygon" are avaible now.
-- `output` : output filenames, default directory is `.snapshot`.
-  - `ownerOf` : filename for tokenIds and their owner addresses
-  - `balanceOf` : filename for owner addresses and token balances
-- `params` : request parameters for alchemy API. Please see https://docs.alchemy.com/alchemy/enhanced-apis/transfers-api .
-  - `fromBlock` : first block number to query
-  - `toBlock` : last block number to query
-  - `contractAddresses` : array of contract addresses
+- `chain` : Snapshot chain, "ethereum" and "polygon" are avaible now.
+- `format` : Select format of numbers. Use "decimal", program automatically change from decimal number to hex number with "0x". This option applied to `fromBlock`, `toBlock` and `maxCount`. When you don't select decimal mode, you have to use hex with "0x".
+- `output` : Output filenames as json, default directory is `.snapshot`.
+  - `ownerOf` : Filename for tokenIds and their owner addresses
+  - `balanceOf` : Filename for owner addresses and token balances
+- `params` : Request parameters for alchemy API. Please see https://docs.alchemy.com/alchemy/enhanced-apis/transfers-api .
+  - `fromBlock` : First block number to query
+  - `toBlock` : Last block number to query
+  - `contractAddresses` : Array of contract addresses
  
   Attension: `category` is specified by this program.
 
 That's all! Enjoy!
 
+## License
 
+Snapshot ERC721 is released under the MIT license.
--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+/*
+ * Snapshot ERC721
+ *
+ * Copyright (c) edy
+ *   https://github.com/cryptedy
+ * 
+ * Released under the MIT license.
+ * see https://opensource.org/licenses/MIT
+ * 
+ * Thanks to Alchemy
+ * 
+ * GitHub repository:
+ * see https://github.com/cryptedy/snapshot-erc721
+ */
+
 require("dotenv").config();
 const axios = require("axios");
 const conf = require("./snapshot.config.json");
@@ -6,23 +21,65 @@ const path = require("path");
 
 let cwd = process.cwd();
 let ownerList={};
+let fnOwner, fnBalance;
 
 const main = async () =>{
-    // set category for erc721
-    conf.params.category = ["erc721"];
     // prepare alchemy endpoint with apikey by chain
     let url;
     switch (conf.chain.toLowerCase()) {
         case "ethereum" :
-            url = process.env.ETHEREUM_URL;
+            url = process.env?.ETHEREUM_URL;
             break;
         case "polygon" :
-            url = process.env.POLYGON_URL;
+            url = process.env?.POLYGON_URL;
             break;
         default:
             console.log("ERROR:invalid chain is specified.")
             process.exit(1);
     }
+
+    // check definition of URL in .env
+    if (!url){
+        console.log("ERROR:URL in .env may be empty. Please check .env file.")
+        process.exit(1);
+    }
+
+    // check parameters in setting
+    if (!conf?.params){
+        console.log("ERROR:params in snapshot.config.json is undefined.")
+        process.exit(1);
+    }
+
+    // set category for erc721
+    conf.params.category = ["erc721"];
+
+    // init file path
+    if (!conf?.output?.ownerOf  || !conf?.output?.balanceOf){
+        console.log("ERROR:output filenames in snapshot.config.json are undefined.")
+        process.exit(1);
+    }
+    fnOwner = path.join(cwd,conf.output.ownerOf);
+    fnBalance = path.join(cwd,conf.output.balanceOf);
+    if (!fs.existsSync(path.dirname(fnOwner))) fs.mkdirSync(path.dirname(fnOwner));
+    if (!fs.existsSync(path.dirname(fnBalance))) fs.mkdirSync(path.dirname(fnBalance));
+
+    // if decimal mode, change numbers from decimal to hex with 0x
+    if (conf?.format == "decimal"){
+        if (conf.params?.fromBlock){
+            if (conf.params?.fromBlock != "latest"){
+                conf.params.fromBlock = "0x" + parseInt(conf.params?.fromBlock).toString(16);
+            }
+        }
+        if (conf.params?.toBlock){
+            if (conf.params?.toBlock != "latest"){
+                conf.params.toBlock = "0x" + parseInt(conf.params?.toBlock).toString(16);
+            }
+        }
+        if (conf.params?.maxCount){
+            conf.params.maxCount = "0x" + parseInt(conf.params?.maxCount).toString(16);
+        }
+    }
+
     // prepare request contents
     let contents = {
         "jsonrpc": "2.0",
@@ -31,14 +88,11 @@ const main = async () =>{
         "params": [ conf.params ]
          
     };
-    // prepare post request header
-    let request = {
-        headers: {
-            "content-Type" : "application/json"
-        },
-        data : contents
-    };
 
+    // inform request data for user's debuging setting file.
+    console.log("Request data as json : ", JSON.stringify(conf.params));
+
+    // execute post loop to alchemy api.
     await requestData(url, contents);
 
 }
@@ -47,26 +101,34 @@ const requestData = async (url, data) =>{
     let final = false;    
     await axios.post(url, JSON.stringify(data))
     .then( async(response) =>{
-        console.log(response.data.result?.pageKey);
+        //check response contents
+        if (!response.data?.result){
+            console.log("ERROR:result of response does not exist.")
+            process.exit(1);
+        }
+        if (!response.data.result?.transfers){
+            console.log("ERROR:Any transfer transactions do not exist.")
+            process.exit(1);
+        }
+        console.log("pageKey : ", response.data.result?.pageKey);
         // At first, check pagenation
         if (typeof response.data.result?.pageKey === "undefined"){
             // final loop
             final = true;
-            console.log("final json");
+            console.log("reading final json");
         } else{
             console.log("reading json");
             // set pageKey in contents
             data.params[0].pageKey = response.data.result?.pageKey;
         }
         // update owner list
-        let loop = true;
         const transfers = response.data.result.transfers;
         let len = transfers.length;
         for (let i = 0 ; i < len ; i++){
             let id = parseInt(transfers[i].tokenId,16).toString();
             ownerList[id] = transfers[i].to;
         }
-        console.log("finish parsing json");
+        console.log("finish parsing json, the amount of transfer:", len);
     })
     .catch( (error) => {
         console.log(error);
@@ -76,11 +138,6 @@ const requestData = async (url, data) =>{
             // recursive post
             requestData(url, data);
         }else{
-            // init file path
-            let fnOwner = path.join(cwd,conf.output.ownerOf);
-            let fnBalance = path.join(cwd,conf.output.balanceOf);
-            if (!fs.existsSync(path.dirname(fnOwner))) fs.mkdirSync(path.dirname(fnOwner));
-            if (!fs.existsSync(path.dirname(fnBalance))) fs.mkdirSync(path.dirname(fnBalance));
             // save ownerOf data 
             fs.writeFileSync(fnOwner, JSON.stringify(ownerList));
             // aggregate balance
@@ -94,13 +151,13 @@ const requestData = async (url, data) =>{
                     balance[addr] += 1;
                 }
             }
+            // save balanceOf data
             fs.writeFileSync(fnBalance, JSON.stringify(balance));
-
+            // inform success of procession
+            console.log("SUCCESS:finish writing the results in the specified filenames.");
         }
     });
 
 }
-
-
 
 main();
